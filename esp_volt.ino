cef0a75a118cd009e660f0fb5c055cd183c8a492
Your Name <you@example.com>
2022-03-02 15:09:23 +0700

    second

---
+++ esp_volt.ino
@@ -0,0 +1,48 @@
+
+#include "EmonLib.h"   //https://github.com/openenergymonitor/EmonLib
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+ 
+EnergyMonitor emon;
+#define vCalibration 40
+#define currCalibration 0.52
+
+ 
+char auth[] = "*********************";
+ 
+char ssid[] = "*********************";
+char pass[] = "*********************3";
+ 
+float kWh = 0;
+unsigned long lastmillis = millis();
+ 
+void myTimerEvent() {
+    emon.calcVI(20, 2000);
+    Serial.print("Vrms: ");
+    Serial.print(emon.Vrms, 2);
+    Serial.print("V");
+    Serial.print("\tIrms: ");
+    Serial.print(emon.Irms, 4);
+    Serial.print("A");
+    Serial.print("\tPower: ");
+    Serial.print(emon.apparentPower, 4);
+    Serial.print("W");
+    Serial.print("\tkWh: ");
+    kWh = kWh + emon.apparentPower*(millis()-lastmillis)/3600000000.0;
+    Serial.print(kWh, 4);
+    Serial.println("kWh");
+    lastmillis = millis();
+}
+ 
+void setup() {
+  Serial.begin(9600);
+  emon.voltage(35, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
+  emon.current(34, currCalibration); // Current: input pin, calibration.
+}
+ 
+void loop() {
+  Serial.print("A");
+myTimerEvent();
+delay(1000);
+}
